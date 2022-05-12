6dbd93740e8e3efff5a0b48cd048534a61256c62
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-05-12 11:47:54 -0300

    applying the Singleton Pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -4,8 +4,18 @@ import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesReposito
 export class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor(){
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor(){
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
@@ -16,7 +26,7 @@ export class CategoriesRepository implements ICategoriesRepository {
       description,
       created_at: new Date()
     });
-  
+
     this.categories.push(category);
   }
 
