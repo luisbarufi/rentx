aebc3f06f30e2077cbd385f428e436093751ecf3
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-05-12 15:48:24 -0300

    adding typing

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -4,7 +4,7 @@ import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 export class CreateSpecificationController {
   constructor( private createSpecificationUseCase: CreateSpecificationUseCase ) {}
 
-  handle(request: Request, response: Response) {
+  handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
     this.createSpecificationUseCase.execute({ name, description });
