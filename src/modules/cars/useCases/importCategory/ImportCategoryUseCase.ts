7b27f95045a57b65e2bc1f90c67f8b0f58db1462
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-05-15 21:38:35 -0300

    inserting the upload data into the repository

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -36,6 +36,18 @@ export class ImportCategoryUseCase {
 
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategoies(file);
-    console.log(categories);
+
+    categories.map( async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name)
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description
+        });
+      }
+    });
   }
 }
