0619767fec9df975400bf2d4b6fcc15d3f2be640
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-05-23 15:01:06 -0300

    creating category migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,12 +2,14 @@ import "dotenv/config";
 import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
+  migrationsTableName: "migrations",
   type: "postgres",
   port: 5444,
   host: process.env.HOST,
   username: "docker",
   password: "ignite",
   database: "rentx",
+  migrations: ["./src/database/migrations/*.ts"],
 });
 
 dataSource.initialize();
