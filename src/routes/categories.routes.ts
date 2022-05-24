aaed99f523e85e89b37a4a697782c311f542c794
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-05-24 16:19:09 -0300

    dependency injection on categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import multer from "multer";
 
-import createCategoryController from "../modules/cars/useCases/createCategory";
-import importCategoryController from "../modules/cars/useCases/importCategory";
-import listCategoriesController from "../modules/cars/useCases/listCategories";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
 
 export const categoriesRotes = Router();
 
@@ -11,14 +11,14 @@ const upload = multer({
   dest: "./tmp",
 });
 
-categoriesRotes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+const createCategoryController = new CreateCategoryController();
+const listCategoriesController = new ListCategoriesController();
+const importCategoryController = new ImportCategoryController();
 
-categoriesRotes.get("/", (request, response) => {
-  return listCategoriesController().handle(request, response);
-});
-
-categoriesRotes.post("/import", upload.single("file"), (request, response) => {
-  return importCategoryController().handle(request, response);
-});
+categoriesRotes.post("/", createCategoryController.handle);
+categoriesRotes.get("/", listCategoriesController.handle);
+categoriesRotes.post(
+  "/import",
+  upload.single("file"),
+  importCategoryController.handle
+);
