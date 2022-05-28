d0d2d5a61496d7851a122ec6b22ce97c2df81789
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-05-28 11:11:18 -0300

    creating user controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 import { categoriesRotes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 export const router = Router();
 
 router.use("/categories", categoriesRotes);
 router.use("/specifications", specificationsRoutes);
+router.use("/users", usersRoutes);
