bf80c49916971ff2be3853321b5f1e5eae7758c4
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-07-03 19:27:58 -0300

    Update ListCategoriesController.ts

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -5,13 +5,9 @@ import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 export class ListCategoriesController {
   async handle(request: Request, response: Response): Promise<Response> {
-    try {
-      const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
-      const categories = await listCategoriesUseCase.execute();
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
+    const categories = await listCategoriesUseCase.execute();
 
-      return response.status(201).json(categories);
-    } catch (err) {
-      return response.status(400).json({ err: err.message });
-    }
+    return response.json(categories);
   }
 }
