db5244ec2d73db09f104140bf9e56fd519acc430
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-07-03 19:27:48 -0300

    Update ListSpecificationsController.ts

--- src/modules/cars/useCases/listSpecifications/ListSpecificationsController.ts
+++ src/modules/cars/useCases/listSpecifications/ListSpecificationsController.ts
@@ -5,16 +5,12 @@ import { ListSpecificationsUseCase } from "./ListSpeficicationsUseCase";
 
 export class ListSpecificationsController {
   async handle(request: Request, response: Response) {
-    try {
-      const listSpecificationsUseCase = container.resolve(
-        ListSpecificationsUseCase
-      );
+    const listSpecificationsUseCase = container.resolve(
+      ListSpecificationsUseCase
+    );
 
-      const specifications = await listSpecificationsUseCase.execute();
+    const specifications = await listSpecificationsUseCase.execute();
 
-      return response.status(200).json(specifications);
-    } catch (err) {
-      return response.status(400).json({ err: err.message });
-    }
+    return response.json(specifications);
   }
 }
