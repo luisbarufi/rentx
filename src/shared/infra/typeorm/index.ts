ba1a77d16c254abcc594627ed0ff72cfd7ed7730
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-07-21 23:34:29 -0300

    refactoring user authentication

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import { DataSource } from "typeorm";
 
 import { User } from "../../../modules/accounts/infra/typeorm/entities/User";
+import { UserTokens } from "../../../modules/accounts/infra/typeorm/entities/UserTokens";
 import { Car } from "../../../modules/cars/infra/typeorm/entities/Car";
 import { CarImage } from "../../../modules/cars/infra/typeorm/entities/CarImage";
 import { Category } from "../../../modules/cars/infra/typeorm/entities/Category";
@@ -18,7 +19,7 @@ export const dataSource = new DataSource({
   password: process.env.PASSWORD,
   database:
     process.env.NODE_ENV === "test" ? process.env.DB_TEST : process.env.DB_DEV,
-  entities: [Category, Specification, User, Car, CarImage, Rental],
+  entities: [Category, Specification, User, Car, CarImage, Rental, UserTokens],
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
 });
 
