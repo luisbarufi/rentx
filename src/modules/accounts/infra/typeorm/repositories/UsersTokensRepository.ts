dd08d57ec706660f69bec32f03afe302a7819fa2
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-07-22 20:38:15 -0300

    adding use case for refresh token

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -27,4 +27,18 @@ export class UsersTokensRepository implements IUsersTokensRepository {
 
     return userToken;
   }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    const userTokens = await this.repository.findOne({
+      where: { user_id, refresh_token },
+    });
+    return userTokens;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
 }
