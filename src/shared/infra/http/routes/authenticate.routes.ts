4e4c8719fd23ffffd4fe09d64d238f9479203a71
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-07-23 15:46:03 -0300

    refactoring authenticate routes

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "../../../../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "../../../../modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 export const authenticationRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticationRoutes.post("/sessions", authenticateUserController.handle);
+authenticationRoutes.post("/refresh-token", refreshTokenController.handle);
